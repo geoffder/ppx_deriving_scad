36212f1069fab2d52994cc6ecdc4fb2f972a322a
Geoff deRosenroll <geoffderosenroll@gmail.com>
2022-06-07 23:35:07 -0700

    WIP: untested dimension checker

--- src/ppx_deriving_scad.ml
+++ src/ppx_deriving_scad.ml
@@ -29,6 +29,75 @@ open! Ast_builder.Default
       dimension for signatures as well as implementations, the space type check
       should be a separate function *)
 
+(* NOTE: if a dim check comes back dimensionless, a ppx error will be raised if
+    the user has not provided an attribute that disambiguates it *)
+type dimension =
+  | Dimensionless
+  | D2
+  | D3
+  | Poly of string * string
+
+let equal_dimensions a b =
+  match a, b with
+  | D2, D2 -> true
+  | D3, D3 -> true
+  | Poly (s1, r1), Poly (s2, r2) -> String.equal s1 s2 && String.equal r1 r2
+  | Dimensionless, _ | _, Dimensionless -> true
+  | _ -> false
+
+type dim_error =
+  | MixedDimensions
+  | PolyCollapse
+  | PolyMismatch
+
+let is_constr = function
+  | { ptyp_desc = Ptyp_constr _; _ } -> true
+  | _ -> false
+
+let dim_check ~loc ct =
+  let rec aux dim = function
+    | [%type: [%t? typ] option]
+    | [%type: [%t? typ] Option.t]
+    | [%type: [%t? typ] list]
+    | [%type: [%t? typ] List.t]
+    | [%type: ([%t? typ], [%t? _]) result]
+    | [%type: ([%t? typ], [%t? _]) Result.t] -> aux dim typ
+    | [%type: (Vec2.t, float) Scad.t]
+    | [%type: (Vec2.t, float) Scad_ml.Scad.t]
+    | [%type: Scad.d2]
+    | [%type: Scad_ml.Scad.d2] ->
+      ( match dim with
+      | D3 -> Error MixedDimensions
+      | Poly _ -> Error PolyCollapse
+      | _ -> Ok D2 )
+    | [%type: (Vec3.t, Vec3.t) Scad.t]
+    | [%type: (Vec3.t, Vec3.t) Scad_ml.Scad.t]
+    | [%type: Scad.d3]
+    | [%type: Scad_ml.Scad.d3] ->
+      ( match dim with
+      | D2 -> Error MixedDimensions
+      | Poly _ -> Error PolyCollapse
+      | _ -> Ok D3 )
+    | [%type:
+        ([%t? { ptyp_desc = Ptyp_var s; _ }], [%t? { ptyp_desc = Ptyp_var r; _ }]) Scad.t]
+    | [%type:
+        ( [%t? { ptyp_desc = Ptyp_var s; _ }]
+        , [%t? { ptyp_desc = Ptyp_var r; _ }] )
+        Scad_ml.Scad.t] ->
+      ( match dim with
+      | D2 | D3 -> Error PolyCollapse
+      | Poly (s', r') when String.equal s s' && String.equal r r' -> Ok (Poly (s, r))
+      | Dimensionless -> Ok (Poly (s, r))
+      | _ -> Error PolyMismatch )
+    | { ptyp_desc = Ptyp_tuple (hd :: cts); _ } ->
+      Result.bind ~f:(fun init -> List.fold_result ~init ~f:aux cts) (aux dim hd)
+    | { ptyp_desc = Ptyp_constr (_, []); _ } -> Ok dim
+    | { ptyp_desc = Ptyp_constr (_, (arg :: _ as args)); _ } ->
+      if List.for_all ~f:(Fn.non is_constr) args then Ok dim else aux dim arg
+    | ct -> Location.raise_errorf ~loc "Unhandled type: %s" (string_of_core_type ct)
+  in
+  aux Dimensionless ct
+
 type transform =
   | Translate
   | Scale
@@ -53,6 +122,7 @@ let transform_to_string = function
   | Quaternion -> "quaternion"
   | QuaternionAbout -> "quaternion_about_pt"
   | Mirror -> "mirror"
+  | MultMatrix -> "multmatrix"
 
 let transform_to_rev_params = function
   | Translate -> [ "p" ]
@@ -62,6 +132,7 @@ let transform_to_rev_params = function
   | Quaternion -> [ "q" ]
   | QuaternionAbout -> [ "p"; "q" ]
   | Mirror -> [ "ax" ]
+  | MultMatrix -> [ "m" ]
 
 let transform_drop_about = function
   | RotateAbout -> Rotate
@@ -121,9 +192,6 @@ let transform_expr ~loc ~jane ~transform ~kind (ct : core_type) =
   and jane =
     (jane || (Option.is_some @@ Attr.get_mapf kind))
     && (not @@ Option.is_some @@ Attr.get_map kind)
-  and is_constr = function
-    | { ptyp_desc = Ptyp_constr _; _ } -> true
-    | _ -> false
   in
   let f (name, params) =
     let inner_expr name lid =
@@ -256,6 +324,14 @@ let transformer_intf ~ctxt (_rec_flag, type_declarations) =
         ~loc
         "Deriving scad transformers for non-abstract/record types is not supported."
     | { ptype_kind = Ptype_abstract | Ptype_record _; ptype_name; ptype_params; _ } ->
+      (* TODO: dimension type checking returning an result of (D2 / D3 /
+                  Poly of (param1, param2) sumtype)
+          - if Ok, proceed, if Error, raise the ppx error.
+          - params for polymorphic type were already taken care of before, but
+            the functions generated on it will have to use the correct parameters
+            (not the non-dimensional parameters to the input type, if it has
+            any). The correct params will be given by the Poly variant mentioned
+            above. *)
       let gen_sig transform =
         let name =
           let func_name = transform_to_string transform in
