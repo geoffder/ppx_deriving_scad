3762494fc5d6525014832695c1c5830856a0278b
Geoff deRosenroll <geoffderosenroll@gmail.com>
2022-06-10 11:54:03 -0700

    Deduplicate with functor

--- src/attr.ml
+++ src/attr.ml
@@ -4,81 +4,40 @@ module type S = sig
   type t
 
   val unit : (t, unit) Attribute.t
-
-  (* val ignore : (t, unit) Attribute.t *)
+  val ignore : (t, unit) Attribute.t
   val map : (t, unit) Attribute.t
   val mapf : (t, unit) Attribute.t
   val d2 : (t, unit) Attribute.t
   val d3 : (t, unit) Attribute.t
 end
 
-module Field = struct
+module M (T : sig
+  type t
+
+  val t : t Attribute.Context.t
+end) =
+struct
+  type t = T.t
+
+  let unit = Attribute.declare "scad.unit" T.t Ast_pattern.(pstr nil) ()
+  let ignore = Attribute.declare "scad.ignore" T.t Ast_pattern.(pstr nil) ()
+  let map = Attribute.declare "scad.map" T.t Ast_pattern.(pstr nil) ()
+  let mapf = Attribute.declare "scad.mapf" T.t Ast_pattern.(pstr nil) ()
+  let d2 = Attribute.declare "scad.d2" T.t Ast_pattern.(pstr nil) ()
+  let d3 = Attribute.declare "scad.d3" T.t Ast_pattern.(pstr nil) ()
+end
+
+module Field : S with type t = label_declaration = M (struct
   type t = label_declaration
 
-  let unit =
-    Attribute.declare
-      "scad.unit"
-      Attribute.Context.label_declaration
-      Ast_pattern.(pstr nil)
-      ()
+  let t = Attribute.Context.label_declaration
+end)
 
-  let ignore =
-    Attribute.declare
-      "scad.ignore"
-      Attribute.Context.label_declaration
-      Ast_pattern.(pstr nil)
-      ()
-
-  let map =
-    Attribute.declare
-      "scad.map"
-      Attribute.Context.label_declaration
-      Ast_pattern.(pstr nil)
-      ()
-
-  let mapf =
-    Attribute.declare
-      "scad.mapf"
-      Attribute.Context.label_declaration
-      Ast_pattern.(pstr nil)
-      ()
-
-  let d2 =
-    Attribute.declare
-      "scad.d2"
-      Attribute.Context.label_declaration
-      Ast_pattern.(pstr nil)
-      ()
-
-  let d3 =
-    Attribute.declare
-      "scad.d3"
-      Attribute.Context.label_declaration
-      Ast_pattern.(pstr nil)
-      ()
-end
-
-module Type = struct
+module Type : S with type t = core_type = M (struct
   type t = core_type
 
-  let unit =
-    Attribute.declare "scad.unit" Attribute.Context.core_type Ast_pattern.(pstr nil) ()
-
-  let ignore =
-    Attribute.declare "scad.ignore" Attribute.Context.core_type Ast_pattern.(pstr nil) ()
-
-  let map =
-    Attribute.declare "scad.map" Attribute.Context.core_type Ast_pattern.(pstr nil) ()
-
-  let mapf =
-    Attribute.declare "scad.mapf" Attribute.Context.core_type Ast_pattern.(pstr nil) ()
-
-  let d2 =
-    Attribute.declare "scad.d2" Attribute.Context.core_type Ast_pattern.(pstr nil) ()
-
-  let d3 =
-    Attribute.declare "scad.d3" Attribute.Context.core_type Ast_pattern.(pstr nil) ()
-end
+  let t = Attribute.Context.core_type
+end)
 
 let get_unit = function
   | `Type ct -> Attribute.get Type.unit ct
